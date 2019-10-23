ae66d4c1c6782e84d63df169d719d0816635c21d
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2019-10-22 21:14:20 -0500

    Create triangle count medium.py

---
+++ triangle count medium.py
@@ -0,0 +1,22 @@
+class Solution:
+    """
+    @param S: A list of integers
+    @return: An integer
+    """
+    def triangleCount(self, S):
+        # write your code here
+        S.sort()
+        ans = 0 
+        for i in range(len(S)-1,1,-1):
+            left, right = 0, i-1
+            while left < right:
+                if S[left] + S[right]>S[i]:
+                    ans += (right-left)
+                    right -=1 
+                        
+                else:
+                    left+= 1 
+                        
+        
+        return ans 
+        
