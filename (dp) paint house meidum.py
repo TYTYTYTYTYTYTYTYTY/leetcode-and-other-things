5eb9cd535928b9ade4ab215aa83e01a212597881
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2019-10-24 10:57:56 -0500

    method2 using 1d list

--- (dp) paint house meidum.py
+++ (dp) paint house meidum.py
@@ -22,3 +22,35 @@ class Solution:
                 
         
         return min(dp[-1])
+
+
+
+
+
+# method 2 less space
+
+class Solution:
+    """
+    @param costs: n x 3 cost matrix
+    @return: An integer, the minimum cost to paint all houses
+    """
+    def minCost(self, costs):
+        # write your code here
+        # total cost include this and options
+        if not costs:
+            return 0 
+            
+        dp = [sys.maxsize] * 3
+        
+        for i, cost in enumerate(costs):
+            if i ==0:
+                dp = cost 
+            
+            else:
+                copy = dp[:]
+                dp[0] = min(copy[1], copy[2]) + cost[0]
+                dp[1] = min(copy[0], copy[2]) + cost[1]
+                dp[2] = min(copy[0], copy[1]) + cost[2]
+                
+        
+        return min(dp)
